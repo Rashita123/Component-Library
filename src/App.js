7da4f8bed60bac24e89b81afa076e6a2e085fc65
Rashita Mehta <56469693+Rashita123@users.noreply.github.com>
2021-03-17 02:02:58 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import { Button } from "./MainArea/Button/Button";
+import { Toast } from "./MainArea/Toast/Toast";
+export default function App() {
+  return (
+    <div className="App">
+      <Button text="Button" type="secondary" size="md" />
+      <Toast text="Success" type="Success" />
+    </div>
+  );
+}
