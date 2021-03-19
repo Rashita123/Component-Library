a34e4153cdd908fbc93634b636267bf1d3c6ebc3
Rashita Mehta <56469693+Rashita123@users.noreply.github.com>
2021-03-20 00:40:25 +0530

    code clean

--- src/Documentation/Documentation.js
+++ src/Documentation/Documentation.js
@@ -3,15 +3,12 @@ import { useDocsContext } from "../ViewContext";
 import { ButtonDocs, InputDocs, AvatarDocs } from "./ComponentDocs";
 export const Documentation = () => {
   const { docsComponentToDisplay } = useDocsContext();
-  console.log(docsComponentToDisplay);
   return (
     <div className="documentation">
+      {docsComponentToDisplay === "" && <AvatarDocs />}
       {docsComponentToDisplay === "Button" && <ButtonDocs />}
       {docsComponentToDisplay === "Input" && <InputDocs />}
       {docsComponentToDisplay === "Avatar" && <AvatarDocs />}
-      {/* <InputDocs /> */}
-      {/* <ButtonDocs /> */}
-      {/* <AvatarDocs /> */}
     </div>
   );
 };
